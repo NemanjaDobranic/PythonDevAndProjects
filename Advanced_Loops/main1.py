c0a40bcc89dcfb72d058fccf06ca708dc9063985
NemanjaDobranic <dobranicnemanja2014@gmail.com>
2021-07-26 20:20:05 +0200

    Advanced Loops Sequel

---
+++ Advanced_Loops/main1.py
@@ -0,0 +1,20 @@
+letter = 'a'
+length = 5
+for i in range(length + 1):
+    print(letter * i)
+
+for i in range(length - 1, 0, -1):
+    print(letter * i)
+
+letter = 'b'
+for i in range(length + 1):
+    print(' ' * (length - i),end='')
+    print(letter * i)
+
+for i in range(length - 1, 0 , - 1):
+    print(' ' * (length - i),end='')
+    print(letter * i)
+
+#going through alpahbet
+#my_char = ord(letter)
+#letter = chr(my_char + i)
