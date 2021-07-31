c063bab60436e529853f213a3d12fd845b33a375
NemanjaDobranic <dobranicnemanja2014@gmail.com>
2021-07-31 22:48:19 +0200

    hiiiiii

--- Projects/Hangman/read_english_dictionary.py
+++ Projects/Hangman/read_english_dictionary.py
@@ -1,5 +1,5 @@
 def load_words():
-    with open('english_words/words_alpha.txt') as word_file:
+    with open('Projects\Hangman\words_alpha.txt') as word_file:
         valid_words = set(word_file.read().split())
     return valid_words
 
